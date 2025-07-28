e02d0eeb3c2870860265319f4f6b7d09f120dd52
Blackscure <wekesabuyahi@gmail.com>
2025-07-28 10:13:42 +0300

    modification

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
 
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "../../../public/assets/Petro_buyahi.pdf"; 
+    link.href = "/assets/Petro_buyahi.pdf"; 
     link.download = "resume.pdf";
     link.click();
 
